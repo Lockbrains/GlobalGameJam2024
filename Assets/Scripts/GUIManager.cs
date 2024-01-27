9124bf20147c56eef4c59e696c396406cd844a49
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2024-01-26 19:07:11 -0500

    Basic Managers
    As Titled.

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    public static GUIManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
