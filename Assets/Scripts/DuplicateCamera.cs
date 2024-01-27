fff55b55ac5da6538312cd0926d22cf984b9836c
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2024-01-27 00:38:06 -0500

    Scene Edit

--- Assets/Scripts/DuplicateCamera.cs
+++ Assets/Scripts/DuplicateCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class DuplicateCamera : MonoBehaviour
 {
     [SerializeField] private Camera _originCam;
