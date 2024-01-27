1d2c6f095be671987c0a87ab2d6a5fdf303fc77a
tsaexu <571418232@qq.com>
2024-01-27 04:13:30 -0500

    updated itchiness measure
    right and left hand each can trigger multiple triggers

--- Assets/Scripts/LeftHand_itchinessMeasure_old.cs
+++ Assets/Scripts/LeftHand_itchinessMeasure_old.cs
@@ -1,25 +1,25 @@
 
 using UnityEngine;
 
-public class RightHand_itchinessMeasure : MonoBehaviour
+public class LeftHand_itchinessMeasure_old : MonoBehaviour
 {
     // Apply this script on both left and right hand
 
     // Drag parent's script to this var
     public PlayerGlobalVar PlayerGlobalVar; //totalItchiness, numOfItchPoint, currentSen, lowSen, midSen, highSen
-    public float rightHandCurrentSen = 0;
-    public LeftHand_itchinessMeasure left;
+    public float leftHandCurrentSen = 0;
+    public RightHand_itchinessMeasure right;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (PlayerGlobalVar.itchiness.ContainsKey(other.tag))
         {
-            rightHandCurrentSen = PlayerGlobalVar.itchiness[other.tag];
+            leftHandCurrentSen = PlayerGlobalVar.itchiness[other.tag];
 
             // In case the next itchy point has a different itchiness
             //PlayerGlobalVar.currentSen = Mathf.Max(PlayerGlobalVar.currentSen, PlayerGlobalVar.itchiness[other.tag]);
-            PlayerGlobalVar.currentSen = Mathf.Max(left.leftHandCurrentSen, rightHandCurrentSen);
+            PlayerGlobalVar.currentSen = Mathf.Max(right.rightHandCurrentSen, leftHandCurrentSen);
 
             // if player is not intersecting with any itchy point
             if (PlayerGlobalVar.numOfItchPoint == 0)
@@ -39,8 +39,8 @@ public class RightHand_itchinessMeasure : MonoBehaviour
     {
         if (PlayerGlobalVar.itchiness.ContainsKey(other.tag))
         {
-            rightHandCurrentSen = 0f;
-            PlayerGlobalVar.currentSen = Mathf.Max(left.leftHandCurrentSen, rightHandCurrentSen);
+            leftHandCurrentSen = 0f;
+            PlayerGlobalVar.currentSen = Mathf.Max(right.rightHandCurrentSen, leftHandCurrentSen);
 
             // if player is only intersecting with one itchy point
             if (PlayerGlobalVar.numOfItchPoint == 1)
--- Assets/Scripts/RightHand_itchinessMeasure.cs
+++ Assets/Scripts/RightHand_itchinessMeasure.cs
@@ -1,14 +1,14 @@
 using System.Linq;
 using UnityEngine;
 
-public class LeftHand_itchinessMeasure : MonoBehaviour
+public class RightHand_itchinessMeasure : MonoBehaviour
 {
-    // Apply this script on left hand
+    // Apply this script on right hand
 
     // Drag parent's script to this var
     public PlayerGlobalVar PlayerGlobalVar;
-    public float leftHandCurrentSen = 0;
-    public RightHand_itchinessMeasure right;
+    public float rightHandCurrentSen = 0;
+    public LeftHand_itchinessMeasure left;
 
     private bool triggerWithLow = false;
     private bool triggerWithMid = false;
@@ -16,7 +16,7 @@ public class LeftHand_itchinessMeasure : MonoBehaviour
     private float lowSen = 0f;
     private float midSen = 0f;
     private float highSen = 0f;
-    public int leftCountSen = 0;
+    public int rightCountSen = 0;
 
     private void OnTriggerStay(Collider other)
     {
@@ -39,10 +39,10 @@ public class LeftHand_itchinessMeasure : MonoBehaviour
                 highSen = PlayerGlobalVar.itchiness["highSen"];
             }
 
-            leftHandCurrentSen = Mathf.Max(lowSen, Mathf.Max(midSen, highSen));
+            rightHandCurrentSen = Mathf.Max(lowSen, Mathf.Max(midSen, highSen));
 
             // In case the next itchy point has a different itchiness
-            PlayerGlobalVar.currentSen = Mathf.Max(right.rightHandCurrentSen, leftHandCurrentSen);
+            PlayerGlobalVar.currentSen = Mathf.Max(left.leftHandCurrentSen, rightHandCurrentSen);
 
 
             // if player is not intersecting with any itchy point
@@ -51,8 +51,8 @@ public class LeftHand_itchinessMeasure : MonoBehaviour
                 PlayerGlobalVar.StartAddItchinenss();
             }
 
-            leftCountSen = CountTrue(triggerWithLow, triggerWithMid, triggerWithHigh);
-            PlayerGlobalVar.numOfItchPoint = leftCountSen + right.rightCountSen;
+            rightCountSen = CountTrue(triggerWithLow, triggerWithMid, triggerWithHigh);
+            PlayerGlobalVar.numOfItchPoint = rightCountSen + left.leftCountSen;
 
         }
     }
@@ -77,8 +77,8 @@ public class LeftHand_itchinessMeasure : MonoBehaviour
                 highSen = 0f;
             }
 
-            leftHandCurrentSen = Mathf.Max(lowSen, Mathf.Max(midSen, highSen));
-            PlayerGlobalVar.currentSen = Mathf.Max(right.rightHandCurrentSen, leftHandCurrentSen);
+            rightHandCurrentSen = Mathf.Max(lowSen, Mathf.Max(midSen, highSen));
+            PlayerGlobalVar.currentSen = Mathf.Max(left.leftHandCurrentSen, rightHandCurrentSen);
 
             // if player is only intersecting with one itchy point
             if (PlayerGlobalVar.numOfItchPoint == 1)
@@ -87,8 +87,8 @@ public class LeftHand_itchinessMeasure : MonoBehaviour
                 PlayerGlobalVar.totalItchiness = 0;
             }
 
-            leftCountSen = CountTrue(triggerWithLow, triggerWithMid, triggerWithHigh);
-            PlayerGlobalVar.numOfItchPoint = leftCountSen + right.rightCountSen;
+            rightCountSen = CountTrue(triggerWithLow, triggerWithMid, triggerWithHigh);
+            PlayerGlobalVar.numOfItchPoint = rightCountSen + left.leftCountSen;
         }
     }
 
